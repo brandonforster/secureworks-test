dc7c1b2395f133d6ce0d72b2e9931179014ee49c
Brandon Forster <me@brandonforster.com>
2020-12-13 14:03:14 -0600

    Building a better toy bikeshed
    Signed-off-by: Brandon Forster <me@brandonforster.com>

--- graph/resolver.go
+++ graph/resolver.go
@@ -13,39 +13,42 @@ type Resolver struct{
 	IPs []*model.IPDetails
 }
 
-func (r Resolver) lookup(IP string) model.IPDetails {
-	var details model.IPDetails
-	exists := false
+func (r Resolver) lookup(IP string) *model.IPDetails {
 	for _, detail := range r.IPs {
 		if IP == detail.IPAddress {
-			details = *detail
-			exists = true
-			break
+			return detail
 		}
 	}
 
-	if !exists {
-		details = model.IPDetails{
-			UUID:         uuid.New().String(),
-			CreatedAt:    time.Now(),
-			UpdatedAt:    time.Now(),
-			ResponseCode: "",
-			IPAddress:    IP,
-		}
-	}
-
-	return details
+	return nil
 }
 
 // TODO: this does not even begin to do what enqueue should
 func (r *Resolver) Store(IP string) bool {
 	details := r.lookup(IP)
-	r.IPs = append(r.IPs, &details)
+	r.IPs = append(r.IPs, details)
 
 	return true
 }
 
 func (r *Resolver) Get(IP string) (*model.IPDetails, error) {
-	retval := r.lookup(IP)
-	return &retval, nil
+	value := r.lookup(IP)
+	if value == nil {
+		return resolve(IP)
+	}
+
+	return value, nil
+}
+
+// TODO: do lookups at spamhaus
+func resolve(IP string) (*model.IPDetails, error) {
+	details := model.IPDetails{
+		UUID:         uuid.New().String(),
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		ResponseCode: "",
+		IPAddress:    IP,
+	}
+
+	return &details, nil
 }
