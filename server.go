5688c325e911c7843727aa1f57e5bb35e734d063
Brandon Forster <me@brandonforster.com>
2020-12-13 22:50:31 -0600

    I will not be thwarted by a stupid gotcha
    Signed-off-by: Brandon Forster <me@brandonforster.com>

--- server.go
+++ server.go
@@ -29,6 +29,7 @@ func main() {
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
 	router.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	router.Handle("/graphql", playground.Handler("GraphQL playground", "/query"))
 	router.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
